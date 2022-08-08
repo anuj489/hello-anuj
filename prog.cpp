1eb80e11667c1b545f4af24ee4682f3dc83ce823
net5 <net5@pdc.jiit>
2022-08-08 14:23:49 +0530

    first-program

---
+++ prog.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+for (int i =0;i<5;i++)
+{
+for (int j=i;j<5;j--)
+{
+cout<<" ";
+
+}
+for(int k=0;k<5;k++)
+{
+cout<<"*";
+}
+cout<<endl;
+}
+
+return 0;
+}
